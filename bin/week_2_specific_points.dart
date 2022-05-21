36040ee2d341bac2f5cd62873ed2f417d785fda9
Omar <omarmeciry21@gmail.com>
2022-05-21 23:57:20 +0200

    init

---
+++ bin/week_2_specific_points.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+  //num x = 3;
+  //x= 3.5;
+
+  //2. const vs final
+  // const double PIE = 3.14;
+  // final double PIE_2 ;
+  // PIE_2=double.parse(stdin.readLineSync()!);
+
+  //3. Dynamic vs Var
+  // var x = false;
+  // // x = 3;
+  // dynamic y = "";
+  // y=3;
+  // y=false;
+
+  //4. Functions / Parameters
+  // List<String> names = ["OMar","Ahmed","Osama"];
+  // List<int> ages = [10,12,14];
+  //
+  // int Max = getMax([5,1564,15,23,1]);
+  // print(Max);
+  // printInfo(names[0], ages[0]);
+  // printInfo(names[1], ages[1]);
+  // printInfo(names[2], ages[2]);
+}
+
+// dynamic getFunction(){
+//   //Unknown data type
+// }
+
+// void printInfo(String name , int age){
+//   print("Name : $name");
+//   print("Age : $age");
+// }
+
+// int getMax(List<int> l){
+//   int m = l[0];
+//
+//   l.forEach((element) {
+//     if(element>m) m = element;
+//   });
+//
+//   return  m;
+// }
